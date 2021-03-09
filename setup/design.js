6f1bda110ddb03542024238fd898d12c4f4345db
abdulatifkhan <anvarkhanov96@gmail.com>
2021-03-09 12:15:05 +0500

    Add settings

---
+++ setup/design.js
@@ -0,0 +1,22 @@
+/* 
+  npm i express apollo-server-express graphql jsonwebtoken moment
+
+  src
+
+    Setting.js
+
+    Library
+
+      Database
+
+        Postgres.js
+
+    Modules
+
+      Server
+
+        Server.js
+        Resolvers.js
+        Schema.js
+
+*/
