1826bfd586bd8cbbb702c096e716c750a9a53905
abdulatifkhan <anvarkhanov96@gmail.com>
2021-03-13 15:29:22 +0500

    Add changes index page

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 const http = require("http")
-const express = require("express")
-const { ApolloServer } = require("apollo-server-express")
+
+const expressServer = require("./src/Express/Application")
+const graphqlServer = require("./src/Apollo")
+
 const { PORT } = require("./src/Settings")
-const modules = require("./src/Modules")
 
-const app = express()
+const httpServer = http.createServer(expressServer)
 
-const server = new ApolloServer({
-  modules,
-  subscriptions: {
-    onConnect: (connectionParams, WebSocket, context) => {},
-    onDisconnect: (WebSocket, context) => {},
-  },
-})
-
-server.applyMiddleware({ app, path: '/graphql' })
-
-const httpServer = http.createServer(app)
-
-server.installSubscriptionHandlers(httpServer)
+graphqlServer.installSubscriptionHandlers(httpServer)
 
 httpServer.listen({ port: PORT }, () => {
-  console.log('http://localhost:' + PORT + server.graphqlPath);
-  console.log('ws://localhost:' + PORT + server.subscriptionsPath);
+  console.log('http://localhost:' + PORT + graphqlServer.graphqlPath);
+  console.log('ws://localhost:' + PORT + graphqlServer.subscriptionsPath);
 })
