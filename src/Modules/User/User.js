bac7f9dc829abdc4d00e0add3561922916905816
abdulatifkhan <anvarkhanov96@gmail.com>
2021-03-15 19:29:26 +0500

    Add user module fixes

--- src/Modules/User/User.js
+++ src/Modules/User/User.js
@@ -1,17 +1,54 @@
 // Model
-const data = []
+const { fetch, fetchOne } = require("../../Library/Database/Postgres")
 
-const many = () => data
-const create = ({ username }) => {
+// const data = []
 
-  const newUser = {
+const MANY = `
+
+  SELECT
+    user_id,
+    user_username,
+    user_first_name,
+    user_last_name,
+    user_middle_name,
+    user_age
+  FROM
+    users
+`
+
+const CREATE = `
+
+  INSERT INTO users (
+
+      user_username,
+      user_password,
+      user_first_name,
+      user_last_name
+
+  ) VALUES (
+    $1, $2, $3, $4
+  ) RETURNING *
+`
+
+const many = async () => {
+  
+  return await fetch(MANY)
+}
+
+const create = async ({ username }) => {
+
+  const newUser = await fetchOne(CREATE, username, "pass1", "fName", "lName")
+
+/*   const newUser = {
     id: data.length + 1,
     username,
   }
 
   data.push(newUser)
 
-  return newUser
+  return newUser */
+
+  return { ... newUser, __typename: "User", }
 }
 
 module.exports.many = many
